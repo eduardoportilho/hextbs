eba6f58128999911b2ca5c73b15c738253bb1519
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-07-12 13:49:03 -0300

    Highlighting hexes on hover

--- main.js
+++ main.js
@@ -1,20 +1,51 @@
+var canvas = document.getElementById('game');
+var ctx = canvas.getContext('2d');
+var gridDimension = 5;
+var edgeSize = 50;
+var origin =  {x: 100, y: 100};
+
+var hexes = {};
+var highlightedHex = undefined;
+
+canvas.onmousemove = function(e) {
+  var mousePosition = {
+    x: e.clientX - e.target.offsetLeft,
+    y: e.clientY - e.target.offsetTop    
+  };
+  var hexOverMouse = getHexContainingPoint(mousePosition);
+  if (!isSameHexCoords(hexOverMouse, highlightedHex)) {
+    highlightedHex = hexOverMouse;
+    draw();
+  }
+};
+
 function draw() {
-  var canvas = document.getElementById('game');
-  var ctx = canvas.getContext('2d');
 
-  var edgeSize = 50,
-    origin =  {x: 100, y: 100};
+  hexes = {};
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (var row = 0; row < gridDimension ; row++) {
+    hexes[row] = {};
 
-  ctx.fillStyle = 'orange';
-  ctx.strokeStyle = 'blue';
+    for (var col = 0; col < gridDimension ; col++) {
+      var isHover = highlightedHex && highlightedHex.col === col && highlightedHex.row === row;
+      style(isHover);
 
-  for (var row = 0; row < 5 ; row++) {
-    for (var col = 0; col < 5 ; col++) {
-      var center = offsetCoordPoint(row, col, edgeSize, origin);
+      var center = getHexCenterPoint(row, col, edgeSize, origin);
       var hex = buildHex(edgeSize, center);
+      hexes[row][col] = hex;
       ctx.fill(hex);
       ctx.stroke(hex);
     }
+  }
+}
+
+function style(hover) {
+  if (hover) {
+    ctx.fillStyle = 'blue';
+    ctx.strokeStyle = 'orange';
+  } else {
+    ctx.fillStyle = 'orange';
+    ctx.strokeStyle = 'blue';
   }
 }
 
@@ -55,7 +86,7 @@ function buildHex(edgeSize, center) {
  * @param  {Point} origin - [X, Y] position of the center of the first (0,0) hex.
  * @return {Point} [X, Y] position of the center of the hex at [row, col].
  */
-function offsetCoordPoint(row, col, edgeSize, origin) {
+function getHexCenterPoint(row, col, edgeSize, origin) {
   origin = origin || {x:0, y:0};
   edgeSize = edgeSize || 50;
   var hexHeight = edgeSize * 2;
@@ -71,4 +102,24 @@ function offsetCoordPoint(row, col, edgeSize, origin) {
     x: origin.x + x,
     y: origin.y + y,
   }
+}
+
+function getHexContainingPoint(point) {
+  for (var row = 0; row < gridDimension ; row++) {
+    for (var col = 0; col < gridDimension ; col++) {
+      var hex = hexes[row][col];
+      if (ctx.isPointInPath(hex, point.x, point.y)) {
+        return {row: row, col: col};
+      }
+    }
+  }
+  return undefined;
+}
+
+function isSameHexCoords(coord1, coord2) {
+  var row1 = coord1 ? coord1.row : undefined,
+      row2 = coord2 ? coord2.row : undefined,
+      col1 = coord1 ? coord1.col : undefined,
+      col2 = coord2 ? coord2.col : undefined;
+  return row1 === row2 && col1 === col2;
 }
