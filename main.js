46092cbf0f4d3811216505cbc7618c5346008cea
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-07-12 10:41:11 -0300

    Drawing multiple hexes

--- main.js
+++ main.js
@@ -2,25 +2,51 @@ function draw() {
   var canvas = document.getElementById('game');
   var ctx = canvas.getContext('2d');
 
+  var edgeSize = 50;
+  var hexHeight = edgeSize * 2;
+  var hexWidth = Math.sqrt(3)/2 * hexHeight;
+  var centerDistance = {
+    x: hexWidth / 2,
+    y: 3 * hexHeight/4
+  }
 
-  var center = {x:100, y:100},
-    cornerPoint = {x:0, y:0},
-    edgeSize = 50,
+  var hex = buildHex(edgeSize);
+
+  ctx.fillStyle = 'orange';
+  ctx.strokeStyle = 'blue';
+
+  ctx.translate(100, 100);
+
+  ctx.fill(hex);
+  ctx.stroke(hex);
+
+  ctx.translate(centerDistance.x, centerDistance.y);
+  ctx.fill(hex);
+  ctx.stroke(hex);
+
+  ctx.translate(centerDistance.x, - centerDistance.y);
+  ctx.fill(hex);
+  ctx.stroke(hex);
+}
+
+function buildHex(edgeSize) {
+  var cornerPoint = {x:0, y:0},
     angleDeg, 
     angleRad;
+  edgeSize = edgeSize || 50;
 
-  ctx.beginPath();
+  var hex = new Path2D();
   for (var cornerIndex = 0; cornerIndex <= 6; cornerIndex++) {
     angleDeg = 60 * cornerIndex + 30;
     angleRad = Math.PI / 180 * angleDeg;
-    cornerPoint.x = center.x + edgeSize * Math.cos(angleRad);
-    cornerPoint.y = center.y + edgeSize * Math.sin(angleRad);
+    cornerPoint.x = edgeSize * Math.cos(angleRad);
+    cornerPoint.y = edgeSize * Math.sin(angleRad);
 
     if (cornerIndex === 0) {
-      ctx.moveTo(cornerPoint.x, cornerPoint.y);
+      hex.moveTo(cornerPoint.x, cornerPoint.y);
     } else {
-      ctx.lineTo(cornerPoint.x, cornerPoint.y);
+      hex.lineTo(cornerPoint.x, cornerPoint.y);
     }
   }
-  ctx.fill();
+  return hex;
 }
