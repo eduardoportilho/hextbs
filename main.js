3801ef2e96f8b6deb9f221030538515f23a6f000
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-07-12 11:12:04 -0300

    Drawing hexes in hives based on offset coordinates.

--- main.js
+++ main.js
@@ -2,45 +2,41 @@ function draw() {
   var canvas = document.getElementById('game');
   var ctx = canvas.getContext('2d');
 
-  var edgeSize = 50;
-  var hexHeight = edgeSize * 2;
-  var hexWidth = Math.sqrt(3)/2 * hexHeight;
-  var centerDistance = {
-    x: hexWidth / 2,
-    y: 3 * hexHeight/4
-  }
-
-  var hex = buildHex(edgeSize);
+  var edgeSize = 50,
+    origin =  {x: 100, y: 100};
 
   ctx.fillStyle = 'orange';
   ctx.strokeStyle = 'blue';
 
-  ctx.translate(100, 100);
-
-  ctx.fill(hex);
-  ctx.stroke(hex);
-
-  ctx.translate(centerDistance.x, centerDistance.y);
-  ctx.fill(hex);
-  ctx.stroke(hex);
-
-  ctx.translate(centerDistance.x, - centerDistance.y);
-  ctx.fill(hex);
-  ctx.stroke(hex);
+  for (var row = 0; row < 5 ; row++) {
+    for (var col = 0; col < 5 ; col++) {
+      var center = offsetCoordPoint(row, col, edgeSize, origin);
+      var hex = buildHex(edgeSize, center);
+      ctx.fill(hex);
+      ctx.stroke(hex);
+    }
+  }
 }
 
-function buildHex(edgeSize) {
+/**
+ * Build a hex path with center on the provided position.
+ * @param  {number} edgeSize - Size of the hex edge.
+ * @param  {Point} center - [X, Y] position of the center of the hex.
+ * @return {Path2D} Hex path.
+ */
+function buildHex(edgeSize, center) {
   var cornerPoint = {x:0, y:0},
     angleDeg, 
     angleRad;
   edgeSize = edgeSize || 50;
+  center = center || {x:0, y:0};
 
   var hex = new Path2D();
   for (var cornerIndex = 0; cornerIndex <= 6; cornerIndex++) {
     angleDeg = 60 * cornerIndex + 30;
     angleRad = Math.PI / 180 * angleDeg;
-    cornerPoint.x = edgeSize * Math.cos(angleRad);
-    cornerPoint.y = edgeSize * Math.sin(angleRad);
+    cornerPoint.x = center.x + edgeSize * Math.cos(angleRad);
+    cornerPoint.y = center.y + edgeSize * Math.sin(angleRad);
 
     if (cornerIndex === 0) {
       hex.moveTo(cornerPoint.x, cornerPoint.y);
@@ -49,4 +45,30 @@ function buildHex(edgeSize) {
     }
   }
   return hex;
+}
+
+/**
+ * Convert an offset cordinate (i.e. hex row and column index) to [X, Y] values. 
+ * @param  {number} row - Hex row index.
+ * @param  {number} col - Hex column index.
+ * @param  {number} edgeSize - Size of the hex edge.
+ * @param  {Point} origin - [X, Y] position of the center of the first (0,0) hex.
+ * @return {Point} [X, Y] position of the center of the hex at [row, col].
+ */
+function offsetCoordPoint(row, col, edgeSize, origin) {
+  origin = origin || {x:0, y:0};
+  edgeSize = edgeSize || 50;
+  var hexHeight = edgeSize * 2;
+  var hexWidth = Math.sqrt(3)/2 * hexHeight;
+  
+  var x = col * hexWidth;
+  var y = row * (3 * hexHeight / 4);
+  if (row % 2 === 0) {
+    x += hexWidth / 2;
+  }
+
+  return {
+    x: origin.x + x,
+    y: origin.y + y,
+  }
 }
