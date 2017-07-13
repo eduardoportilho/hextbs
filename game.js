fc09f7c37f6457fc2b1f19f1d00332c04ff52974
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-07-13 15:50:54 -0300

    Implement attack using risk logic.

--- game.js
+++ game.js
@@ -41,35 +41,68 @@ Game.prototype.move = function(targetCell) {
 };
 
 Game.prototype.attack = function(targetCell) {
-  // assume that targetCell is occupied
-  var attackingPopulation = this.allocateAdjacentPopulation(targetCell, Game.PLAYER_ID, true);
-  var deffendingPopulation = targetCell.population;
-  var battleResult = this.battleResult(attackingPopulation, deffendingPopulation);
+  var attackerId = Game.PLAYER_ID;
+  var attackingCells = this.board.getAdjacentCells(targetCell)
+    .filter(function(adjacent) {
+      return adjacent.player === attackerId &&
+        adjacent.population > 1;
+    });
 
-  targetCell.population = battleResult.survivors;
-  if (battleResult.attackWin) {
-    targetCell.player = Game.PLAYER_ID;
+  var defendingPopulation = targetCell.population;
+  for (var i = 0 ; i < attackingCells.length ; i++) {
+    var attackerCell = attackingCells[i];
+    var attackingPopulation = attackerCell.population - 1;
+    var result = this.riskAttack(attackingPopulation, defendingPopulation);
+
+    attackingPopulation -= result.attackerKills;
+    if (result.attackWin) {
+      attackerCell.population = 1;
+      targetCell.population = attackingPopulation;
+      targetCell.player = attackerId;
+      break;
+    } else {
+      defendingPopulation -= result.defenderKills;
+      attackerCell.population = attackingPopulation + 1;
+      targetCell.population = defendingPopulation;
+    }
   }
 };
 
-
-Game.prototype.battleResult = function(attackingPopulation, deffendingPopulation) {
-  // TODO: implement battle logic
-  var atackWin = Random.yesOrNo();
-  var survivors = Random.getRandomInt(1, (atackWin ? attackingPopulation : deffendingPopulation));
+/**
+ * Simulate attack using risk logic: each atacker-defesor pair generates a dice roll and 
+ * only one will survive. The attack is successfull if all defensors die.
+ * @param  {number} attackingPopulation
+ * @param  {number} defendingPopulation
+ * @return {Object} result - Result object.
+ *         {number} result.attackerKills - How many attacker died?
+ *         {number} result.defenderKills - How many defensors died?
+ *         {boolean} result.attackWin - Attack successfull?
+ */
+Game.prototype.riskAttack = function(attackingPopulation, defendingPopulation) {
+  var attacks = Math.min(attackingPopulation, defendingPopulation);
+  var attackerKills = 0, defenderKills = 0;
+  while (attacks-- > 0) {
+    if (Random.yesOrNo()) {
+      defenderKills++;
+    } else {
+      attackerKills++;
+    }
+  }
   return {
-    attackWin: atackWin,
-    survivors: survivors
-  };
+    attackerKills: attackerKills,
+    defenderKills: defenderKills,
+    attackWin: defenderKills === defendingPopulation
+  }
 }
 
 /**
  * Subtract half of the population of all player cells adjacent
  * to target and return the total subtracted.
  * @param  {Cell} targetCell - Destination cell.
- * @return {number} Subtracted population
+ * @param  {number} playerId - Player of interest.
+ * @return {number} Subtracted population.
  */
-Game.prototype.allocateAdjacentPopulation = function(targetCell, playerId, attack) {
+Game.prototype.allocateAdjacentPopulation = function(targetCell, playerId) {
   var adjcentPlayerCells = this.board.getAdjacentCells(targetCell)
     .filter(function(adjacent) {
       return adjacent.player === playerId &&
@@ -78,9 +111,7 @@ Game.prototype.allocateAdjacentPopulation = function(targetCell, playerId, attac
 
   var movingTotal = 0;
   adjcentPlayerCells.forEach(function (playerCell) {
-    var moving = attack ? 
-      playerCell.population - 1 :
-      Math.floor(playerCell.population / 2);
+    var moving = Math.ceil(playerCell.population / 2);
     movingTotal += moving;
     playerCell.population -= moving;
   });
