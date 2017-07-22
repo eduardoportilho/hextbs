edc9cbfb5cf097419ad48fe9ce64856fef0f8d81
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-07-22 12:06:46 -0300

    Click on empty space, random NPC actions

--- src/random.js
+++ src/random.js
@@ -17,7 +17,7 @@ Random.getRandomIntInclusive = function(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
 };
 
-Random.getRandom = function(array) {
+Random.getRandomElementFromArray = function(array) {
   var index = Random.getRandomIntExclusive(0, array.length);
   return array[index];
 };
